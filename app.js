7091965f2989fbc1b9cb854d1ebfb6d52f35ff44
moohh91 <moohh91@gmail.com>
2015-08-26 23:41:11 -0400

    Initial commit.

---
+++ app.js
@@ -0,0 +1,57 @@
+(function() {
+    var app = angular.module("app", ["ui.router"]);
+
+    app.config([
+        "$stateProvider",
+        "$urlRouterProvider",
+        function($stateProvider, $urlRouterProvider) {
+            $stateProvider
+                .state("home", {
+                    url: "/home",
+                    templateUrl: "/home.html",
+                    controller: "HomeController"
+                })
+                .state("posts", {
+                    url: "/posts/{id}",
+                    templateUrl: "/posts.html",
+                    controller: "PostsController"
+                });
+
+            $urlRouterProvider.otherwise("home");
+        }
+    ]);
+
+    app.controller("AppController", [
+        "$scope",
+        function($scope) {
+
+            $scope.posts = [
+                "1",
+                "2",
+                "3",
+                "4",
+                "5"
+            ];
+
+            $scope.click = function() {
+                $scope.posts = [
+                    "6",
+                    "7",
+                    "8",
+                    "9",
+                    "0"
+                ];
+            };
+        }
+    ]);
+
+    app.controller("PostsController", [
+        "$scope",
+        "$stateParams",
+        "posts",
+        function($scope, $stateParams, posts) {
+
+        }
+    ]);
+
+})();
